d1b39b872fd53b239fbe1a95696e163dab1903a3
bwv988 <ralph.schlosser@gmail.com>
2022-07-24 20:06:52 +0100

    Nested here documents.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 # This is custom data script is executed via cloud-init.
-# Logs are here: /var/log/cloud-init-output.log
+# View logs:
+# tail -f /var/log/cloud-init-output.log
 
-echo -e "\nVM setup script STARTED"
+# Change the username as needed.
+export THE_USER="sral"
+
+echo -e "\nVM setup script STARTED.\nRunning for user ${THE_USER}."
 
 # Update and install packages.
+# FIXME: This should be moved into a proper cloud-init template.
 apt-get update && \
 apt-get upgrade -y && \
 apt install -y build-essential  && \
 apt install -y mc  && \
 apt-get clean
 
-# Install miniconda.
-export CONDA_DIR="/opt/conda"
+# Install miniconda for the user.
+su ${THE_USER} <<'EOF'
+
+export CONDA_DIR="/home/${THE_USER}/opt/conda"
+
 wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
 /bin/bash ~/miniconda.sh -b -p ~/opt/conda
 
 export PATH="~/opt/conda/bin:$PATH"
 
-# Install conda packages.
+# Install required conda packages.
 conda install jupyterlab ipywidgets
-
 conda install pytorch torchvision torchaudio cpuonly -c pytorch
-
 pip install transformers==4.20.1
 
-cat <<'EOF' >>~/.bashrc
-# Adding CONDA to execution path.
+# Add to the user's runtime path.
+echo -e "\n\nexport PATH='~/opt/conda/bin:$PATH'" >> ~/.bashrc
 
-export PATH="~/opt/conda/bin:$PATH"
-EOF
-
-# Run this code to kick off downloading the BLOOM model.
-python - <<'EOF'
+# Run this code to kick-off downloading the BLOOM model.
+python - <<'NESTED'
 # We run this to download / cache the BLOOM model so it's available for later use.
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis", 
-                      model = "bigscience/bloom-1b3")
+                    model = "bigscience/bloom-1b3")
+NESTED
 EOF
 
 echo -e "\nVM setup script COMPLETED."
